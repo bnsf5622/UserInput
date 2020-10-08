dee4c9c800c46549dfa5692f9831205f7b6483d9
chuck francis <chuck_francis10@yahoo.com>
2020-10-08 12:47:21 -0400

    Add project files.

---
+++ UserInput/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UserInput
+{
+    public class UserInput
+    {
+        public static int GetIntFromUser(string prompt)
+        {
+            int result;
+            string userInput;
+
+            // loop until we return an int
+            while (true)
+            {
+                // 1 & 2: Prompt and Read
+                Console.Write(prompt);
+                userInput = Console.ReadLine();
+
+                // attempt to convert
+                if (int.TryParse(userInput, out result))
+                {
+                    // good input;
+                    return result;
+                }
+                // bad input
+                Console.WriteLine("That isd not a valid input.");
+            }
+        }
+    }
+}
